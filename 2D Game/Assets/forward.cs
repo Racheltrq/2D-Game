cf5e02870e43d43ec449e3275275b81624f00d19
Racheltrq <rt1726@nyu.edu>
2019-01-01 16:30:06 +0800

    camera movement

--- 2D Game/Assets/forward.cs
+++ 2D Game/Assets/forward.cs
@@ -16,13 +16,16 @@ public class forward : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-        Vector2 current_player = player.transform.position;
         float transformX = offset[0];
         float transformY = offset[1];
-        transform.position = new Vector3(current_player[0]+transformX, current_player[1]+transformY, -10);
-        
+        Vector2 current_player = player.transform.position;
+        if (((current_player[1]+transformY) < 0.4f) && ((current_player[1] + transformY) > -0.8f))
+            transform.position = new Vector3(current_player[0]+transformX, current_player[1]+transformY, -10);
+        else if ((current_player[1] + transformY) < -0.8f)
+            transform.position = new Vector3(current_player[0] + transformX, -0.8f, -10);
+        else transform.position = new Vector3(current_player[0] + transformX, 0.4f, -10);
+
     }
 }
